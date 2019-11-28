81b23853407cbc1ad55856dcc4272c78baf5e121
twei0407 <47849840+twei0407@users.noreply.github.com>
2019-11-27 22:49:16 -0800

    Fixed scoring

--- src/THI.java
+++ src/THI.java
@@ -187,9 +187,10 @@ public class THI extends JFrame
 			public void actionPerformed(ActionEvent e){
 				for (int i = 0; i < values.length; i++)
 					sum += values[i];
-				function = values[0] + values[1] + values[3] + values[6] + values[8] + values[11] + values[12] + 
-						values[14] + values[17] + values[19] + values[23]; 
-				emotion = values[2] + values[6] + values[9] + values[13] + values[15] + values[16] + values[20] + values[21] + values[24];
+				function = values[0] + values[1] + values[3] + values[6] + values[8] + values[11] + values[12]
+						+ values[14] + values[17] + values[19] + values[23]; 
+				emotion = values[2] + values[5] + values[9] + values[13] + values[15] + values[16] + values[20] 
+						+ values[21] + values[24];
 				catastrophic = values[4] + values[7] + values[10] + values[18] + values[22];
 				System.out.println("total " + sum);
 				System.out.println("function " + function);
